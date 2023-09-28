bf082b53972e0d9872c38fa444953f8229e2e7ac
Arturo Avalos <137134833+oruava@users.noreply.github.com>
2023-09-28 10:22:01 -0300

    Add files via upload
    Realizado metodo hacerReserva, pero solo cambia la disponibilidad hasta ahora

---
+++ Avalos.java
@@ -0,0 +1,54 @@
+package org.example;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Avalos {
+    public static void main(String[] args) {
+
+    }
+
+    public static int ingresarNumeroPieza() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la pieza que quiera reservar");
+        int numeroPieza = sc.nextInt();
+        return numeroPieza-1;
+    }
+    public static String ingresarEstado(){
+        Scanner sc = new Scanner(System.in);
+        boolean condicion=true;
+        String estado="";
+        while(true) {
+            System.out.println("Ingrese si quiere la habitacion reservarla (R), usarla con alimentacion(OA) o usarla sin alimentacion (OS)");
+            estado = sc.next();
+            if (Objects.equals(estado, "R") || Objects.equals(estado, "OA") || Objects.equals(estado, "OS")){
+                break;
+            }
+            else{
+                System.out.println("Respuesta no valida");
+            }
+        }
+        return estado;
+    }
+    public static String [][] hacerReserva(String[][]Piezas, int numeroPieza, String estado){
+        if(verDisponibilidad(numeroPieza,Piezas)){
+            Piezas[1][numeroPieza]=estado;
+        }
+        else if (verDisponibilidad(numeroPieza,Piezas)==false) {
+            System.out.println("La pieza no esta disponible");
+        }
+        return Piezas;
+    }
+
+    public static boolean verDisponibilidad(int habitacion,String[][]Piezas){
+        boolean condicion;
+        if (Piezas[1][habitacion]=="D"){
+            condicion=true;
+        }
+        else {
+            condicion=false;
+        }
+        return condicion;
+    }
+
+}
