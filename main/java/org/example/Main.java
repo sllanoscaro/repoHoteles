0966d65f76f382b207d42748ac6586b4b671b713
Lian Canio <138495779+ChiaxNiro@users.noreply.github.com>
2023-09-28 09:57:37 -0300

    Add files via upload

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -4,22 +4,24 @@ public class Main {
     public static void main(String[] args) {
         String[][] matriz = {
                 {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},
-                {"D", "O", "D", "O", "O", "D", "D", "D", "D", "D"},
+                {"D", "O", "D", "OS", "O", "D", "D", "D", "D", "D"},
                 {"5", "2", "7", "3", "1", "4", "6", "8", "9", "10"}
     };
-    imprimirBoleta(matriz);
+    printMatriz(matriz);
+    imprimirBoleta(matriz,4);
     resetearHotel(matriz);
-        System.out.println("Nuevo hotel");
-    imprimirBoleta(matriz);
+    System.out.println("Nuevo hotel");
+    printMatriz(matriz);
 
     }
-    public static void resetearHotel(String[][] matriz) {
-        for (int i = 0; i < matriz[1].length; i++) {
+    public static String[][] resetearHotel(String[][] matriz) {
+        for (int i = 0; i < 10; i++) {
             matriz[1][i] = "D";
             matriz[2][i] = "0";
         }
+        return matriz;
     }
-    public static void imprimirBoleta(String[][] matriz) {
+    public static void printMatriz(String[][] matriz) {
         int filas = matriz.length;
         int columnas = matriz[0].length;
         for (int i = 0; i < filas; i++) {
@@ -29,5 +31,21 @@ public class Main {
             System.out.println();
         }
     }
+    public static int imprimirBoleta(String[][] matriz, int habitacion){
+        int boleta = 0;
+        if (habitacion > 0 && habitacion < 11) {
+            String estado = matriz[1][habitacion - 1];
+            int dias = Integer.parseInt(matriz[2][habitacion - 1]);
+            if (estado.equals("OA")) {
+                boleta = 45000 * dias;
+            } else if (estado.equals("OS")) {
+                boleta = 30000 * dias;
+            }
+        } else {
+            System.out.println("Número de habitación no válido");
+        }
+        return boleta;
+    }
+    }
 
-}
+
